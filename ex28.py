b10b51772a4db455dabe48325fcb3a5326ec471f
InsupLee <dlstjq0711@gmail.com>
2018-03-22 14:50:27 +0900

    ex28. add numbers

--- ex28.py
+++ ex28.py
@@ -11,11 +11,33 @@ ex28.py
 """
 
 
-if __name__ == '__main__':
+# error handling is implemented
+def add_number(num_of_numbers):
     total = 0
 
-    for i in range(5):
-        num = int(input('Enter a number: '))
-        total += num
+    for i in range(num_of_numbers):
+        try:
+            input_num = int(input('Enter a number: '))
+            total += input_num
+        except ValueError:
+            pass
 
+    return total
+
+
+def main():
+    # input
+    try:
+        num_of_numbers = int(input('Enter the number of numbers to add: '))
+    except ValueError:
+        print('invalid value')
+        return None
+
+    # process
+    total = add_number(num_of_numbers)
+
+    # output
     print('The total is {}.'.format(total))
+
+if __name__ == '__main__':
+    main()
