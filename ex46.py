146a2b884e76d60e77b1246a47c0b6a1cd1717ae
InsupLee <dlstjq0711@gmail.com>
2018-03-23 21:12:38 +0900

    ex46. count word-frequency

--- ex46.py
+++ ex46.py
@@ -7,11 +7,13 @@
 
 """
 ex46.py
-단어 빈도 탐색
+count word-frequncy
 """
 
+import myutil
 
-FILE_NAME = 'ex46_wordfile.txt'
+
+FILE_NAME = 'ex46_wordlist.txt'
 
 
 def count_word_frequency(file_name):
@@ -20,31 +22,31 @@ def count_word_frequency(file_name):
 
     word_list = tmp_data.replace('\n', ' ').split(' ')
 
+    word_dict_keys = list(set(word_list))
+
     # create dictionary based on word_list
     word_dict = dict()
-    for word in word_list:
-        if word in word_dict.keys():
-            word_dict[word] += 1
-        else:
-            word_dict[word] = 1
+
+    for key in word_dict_keys:
+        word_dict[key] = word_list.count(key)
 
     # sort by dictionary value : number of frequency
-    sorted_wd = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_wd_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
 
-    # max_word_len is used for readability(prettier print)
-    max_word_len = max(list(map(lambda x: len(x[0]), sorted_wd))) + 1
-    for word_info in sorted_wd:
-        print('{word_name}:{space}{word_frequency}'.format(
-            word_name=word_info[0],
-            space=' ' * (max_word_len - len(word_info[0])),
-            word_frequency='*' * word_info[1]
-        ))
+    # widths are used for prettier print
+    width1 = max(list(map(lambda x: len(x[0]), sorted_wd_list)))
+    width2 = max(list(map(lambda x: x[1], sorted_wd_list)))
+
+    # output
+    for word_info in sorted_wd_list:
+        # 1 in (width + 1) means length of ':'
+        myutil.print_row([word_info[0] + ':', '*' * word_info[1]], [width1+1, width2+1], sep=' ')
 
 
 def main():
     global FILE_NAME
-
     count_word_frequency(FILE_NAME)
+
 
 if __name__ == '__main__':
     main()
